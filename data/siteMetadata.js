57843eab69f9e90beec7c49df1502cf755a101fb
Alfred-Lau <jian.liu@icloud.com>
2024-08-11 23:48:35 +0800

    feat: add langchain post

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,26 +1,26 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: '榆关',
-  author: '榆关',
-  headerTitle: '榆关的博客',
-  description: '保持深度，保持体系，日拱一卒，功不唐捐',
-  language: 'zh-cn',
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://datafun.bitou.tech/',
-  siteRepo: 'https://github.com/Alfred-Lau',
+  title: "榆关",
+  author: "榆关",
+  headerTitle: "榆关",
+  description: "保持深度，保持体系",
+  language: "zh-cn",
+  theme: "system", // system, dark or light
+  siteUrl: "https://datafun.bitou.tech/",
+  siteRepo: "https://github.com/Alfred-Lau",
   siteLogo: `https://render.bitou.tech/imgs/avatar.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'jian.liu@icloud.com',
-  github: 'https://github.com/Alfred-Lau',
-  x: 'https://twitter.com/x',
+  socialBanner: `${process.env.BASE_PATH || ""}/static/images/twitter-card.png`,
+  mastodon: "https://mastodon.social/@mastodonuser",
+  email: "jian.liu@icloud.com",
+  github: "https://github.com/Alfred-Lau",
+  x: "https://twitter.com/x",
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  locale: 'zh-CN',
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com",
+  linkedin: "https://www.linkedin.com",
+  threads: "https://www.threads.net",
+  instagram: "https://www.instagram.com",
+  locale: "zh-CN",
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
@@ -48,14 +48,14 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: "buttondown",
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: "giscus", // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
@@ -63,27 +63,27 @@ const siteMetadata = {
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      mapping: "pathname", // supported options: pathname, url, title
+      reactions: "1", // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
+      metadata: "0",
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: "light",
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      darkTheme: "transparent_dark",
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
+      themeURL: "",
       // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      lang: "en",
     },
   },
   search: {
-    provider: 'kbar', // kbar or algolia
+    provider: "kbar", // kbar or algolia
     kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
+      searchDocumentsPath: `${process.env.BASE_PATH || ""}/search.json`, // path to load documents to search
     },
     // provider: 'algolia',
     // algoliaConfig: {
@@ -94,6 +94,6 @@ const siteMetadata = {
     //   indexName: 'docsearch',
     // },
   },
-}
+};
 
-module.exports = siteMetadata
+module.exports = siteMetadata;
